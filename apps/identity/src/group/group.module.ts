8f18b7f837a695902c10dfd7405b4cad17a08ab2
IMnhvjp <levanminh19102003@gmail.com>
2025-04-17 17:08:35 +0700

    update group module

--- apps/identity/src/group/group.module.ts
+++ apps/identity/src/group/group.module.ts
@@ -3,11 +3,12 @@ import { GroupService } from './group.service';
 import { GroupController } from './group.controller';
 import { UserGroup } from '../group/entities/user_group.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { GroupRepository } from './group.repository';
 @Module({
-    imports: [
-      TypeOrmModule.forFeature([UserGroup]),
-    ],
-  providers: [GroupService],
+  imports: [
+    TypeOrmModule.forFeature([UserGroup]),
+  ],
+  providers: [GroupRepository, GroupService],
   controllers: [GroupController]
 })
 export class GroupModule {}
