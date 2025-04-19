ba3b1c522f67028a164d2e9781cef7dfca196888
IMnhvjp <levanminh19102003@gmail.com>
2025-04-19 23:50:39 +0700

    define enum UserRole

--- apps/identity/src/user/entities/user.entity.ts
+++ apps/identity/src/user/entities/user.entity.ts
@@ -1,33 +1,43 @@
 import { AbstractEntity } from "@app/common";
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
-import { MemberGroup } from "../../member_group/enitities/member_group.entity";
+import { Column, Entity, OneToMany } from "typeorm";
+import { MemberGroup } from "../../member_group/enities/member_group.entity";
+
+export enum UserRole {
+  ADMIN = 'admin',
+  ADMINI = 'admini',
+  USER = 'user'
+}
 
 @Entity()
 export class User extends AbstractEntity {
-  @Column({ type: "varchar", unique: true, nullable: false })
-  email: string;
+    @Column({ type: "varchar", unique: true, nullable: false })
+    email: string;
 
-  @Column({ type: "varchar", nullable: true })
-  phoneNumber: string;
+    @Column({ type: "varchar", nullable: true })
+    phoneNumber: string;
 
-  @Column({ type: "varchar", nullable: false })
-  password: string;
+    @Column({ type: "varchar", nullable: false })
+    password: string;
 
-  @Column({ type: "varchar", nullable: false })
-  username: string;
+    @Column({ type: "varchar", nullable: false })
+    username: string;
 
-  @Column({ nullable: true })
-  address: string;
+    @Column({ nullable: true })
+    address: string;
 
-  @Column({type: "varchar", nullable: true,})
-  refreshToken: string;
+    @Column({type: "varchar", nullable: true,})
+    refreshToken: string;
 
-  @Column({type: "varchar", nullable: true,})
-  emailCode: string;
+    @Column({type: "varchar", nullable: true,})
+    emailCode: string;
 
-  @Column({ type: "varchar", default: "user"})
-  role: string;
+    @Column({
+        type: 'enum',
+        enum: UserRole,
+        default: UserRole.USER
+    })
+    role: string;
 
-  @OneToMany(() => MemberGroup, memberGroup => memberGroup.group)
-  memberGroups: MemberGroup[];
+    @OneToMany(() => MemberGroup, memberGroup => memberGroup.user)
+    memberships: MemberGroup[];
 }
