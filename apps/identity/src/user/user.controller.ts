8a14b1123c22a0c9e4f90bba31c7b3233a36d0a0
IMnhvjp <levanminh19102003@gmail.com>
2025-04-21 23:36:50 +0700

    add validate_user, find_user_by_id, find_user_by_email msg pattern

--- apps/identity/src/user/user.controller.ts
+++ apps/identity/src/user/user.controller.ts
@@ -6,11 +6,16 @@ import { UserIdParam } from './dto/userId.param';
 import { UpdateUserRequest } from './dto/updateUser.request';
 import { UserInfoResponse } from './dto/userInfo.response';
 import { plainToInstance } from 'class-transformer';
+import { RmqService } from '@app/common';
+import { Ctx, MessagePattern, Payload, RmqContext } from '@nestjs/microservices';
 
 @Controller('user')
 // @UsePipes(new ValidationPipe({ transform: true }))
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly rmqService: RmqService
+  ) {}
 
   @Get()
   async getAllUsers() {
@@ -45,5 +50,59 @@ export class UserController {
   @Delete(':id')
   async deleteUser(@Param() id: UserIdParam) {
     return this.userService.delete(id);
+  }
+
+  @MessagePattern('validate_user')
+  async validateUser(
+    @Payload() data: { email: string; password: string },
+    @Ctx() context: RmqContext,
+  ) {
+    const { email, password } = data;
+    try {
+      const user = await this.userService.findOneByEmail({ email });
+      // if (!user) return null;
+      
+      const isPasswordValid = await this.userService.validatePassword(user, password);
+      if (!isPasswordValid) return null;
+      
+      const { password: _, ...result } = user;
+      this.rmqService.ack(context);
+      return result;
+    } catch (error) {
+      this.rmqService.ack(context);
+      return null;
+    }
+  }
+
+  @MessagePattern('find_user_by_id')
+  async findUserById(
+    @Payload() userId: string,
+    @Ctx() context: RmqContext,
+  ) {
+    try {
+      const user = await this.userService.findOneById({ id: userId });
+      const { password: _, ...result } = user;
+      this.rmqService.ack(context);
+      return result;
+    } catch (error) {
+      this.rmqService.ack(context);
+      return null;
+    }
+  }
+
+  @MessagePattern('find_user_by_email')
+  async findUserByEmail(
+    @Payload() email: string,
+    @Ctx() context: RmqContext,
+  ) {
+    try {
+      const user = await this.userService.findOneByEmail({ email });
+      const { password: _, ...result } = user;
+      this.rmqService.ack(context);
+      return result;
+    } catch (error) {
+      this.rmqService.ack(context);
+      return null;
+    }
   }
 }
