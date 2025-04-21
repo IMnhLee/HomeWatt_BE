358caa1862adefb826973e9eb4e4c5c2f4f2f6fa
IMnhvjp <levanminh19102003@gmail.com>
2025-04-21 22:47:33 +0700

    setup identity microservice as a message handler

--- apps/identity/src/identity.module.ts
+++ apps/identity/src/identity.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
-import { DatabaseModule } from '@app/common';
+import { DatabaseModule, RmqModule } from '@app/common';
 import { UserModule } from './user/user.module';
 import { GroupModule } from './group/group.module';
 import { MemberGroupModule } from './member_group/member_group.module';
@@ -17,10 +17,13 @@ import { MemberGroupModule } from './member_group/member_group.module';
         DB_USERNAME: Joi.string().required(),
         DB_PASSWORD: Joi.string().required(),
         DB_NAME: Joi.string().required(),
+        RABBIT_MQ_URI: Joi.string().required(),
+        RABBIT_MQ_IDENTITY_QUEUE: Joi.string().required(),
       }),
       envFilePath: './apps/identity/.env',
     }),
     DatabaseModule,
+    RmqModule,
     UserModule,
     GroupModule,
     MemberGroupModule,
--- apps/identity/src/main.ts
+++ apps/identity/src/main.ts
@@ -2,11 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { IdentityModule } from './identity.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { RmqService } from '@app/common';
+import { MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(IdentityModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  
   const configService = app.get(ConfigService);
+  const rmqService = app.get<RmqService>(RmqService);
+  
+  // Set up as microservice
+  app.connectMicroservice<MicroserviceOptions>(
+    rmqService.getOptions('IDENTITY', true)
+  );
+  
+  await app.startAllMicroservices();
   await app.listen(configService.get('PORT') ?? 3000);
-  }
+}
 bootstrap();
