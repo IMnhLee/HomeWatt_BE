292a793d529aed431ec7a4229ea83122cdcf2871
IMnhvjp <levanminh19102003@gmail.com>
2025-04-23 02:49:43 +0700

    add export types in libs

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -4,5 +4,6 @@ export * from './database/abstract.entity';
 export * from './database/repository.interface';
 export * from './rmq/rmq.module';
 export * from './rmq/rmq.service';
-// export * from './auth/auth.module';
-// export * from './auth/jwt-auth.guard';
+export * from './auth/auth.module';
+export * from './auth/jwt-auth.guard';
+export * from './types';
